8df4ce3a02cba75ae7d1d11d396326331108e9fd
Dop1ta <gabrielrburgos@outlook.com>
2023-04-15 20:24:48 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
-// import './App.css'
-import { Grid, Box, CardMedia, List, ListItem, Button, Stack } from '@mui/material';
+import { Grid, Box, Card, CardMedia, List, CardActions, Button, CardContent, Typography, Stack } from '@mui/material';
 import axios from "axios";
 
 function App() {
-  // const [perro, setLista] = useState([])
-
-  const [rechazado, setListadoR] = useState([]);
-  const [aceptado, setListadoA] = useState([]);
+  const [rechazado, setListadoR] = useState({
+    link: [],
+    name: []
+  });
+  const [aceptado, setListadoA] = useState({
+    link: [],
+    name: []
+  });
   const [imagenPerro, setImagenPerro] = useState(null);
+  const [NombreP, setNombreP] = useState('');
+
 
   const obtenerImagenPerro = () => {
     axios
@@ -20,81 +25,141 @@ function App() {
 
   useEffect(() => {
     obtenerImagenPerro();
+    generateRandomString();
   }, []);
 
-
   //ACEPTADO EN EL MATCH
-  const aceptarP = (itemURL) => {
-    setListadoA((aceptado) => [...aceptado, itemURL]);
+  const aceptarP = (itemURL, itemName) => {
+    setListadoA((rechazadoAnterior) => ({
+      link: [...rechazadoAnterior.link, itemURL],
+      name: [...rechazadoAnterior.name, itemName]
+    }));
     obtenerImagenPerro();
+    generateRandomString();
   };
 
   //RECHAZADO EN EL MATCH
-  const rechazarP = (itemURL) => {
-    setListadoR((rechazado) => [...rechazado, itemURL]);
+  const rechazarP = (itemURL, itemName) => {
+    setListadoR((rechazadoAnterior) => ({
+      link: [...rechazadoAnterior.link, itemURL],
+      name: [...rechazadoAnterior.name, itemName]
+    }));
     obtenerImagenPerro();
+    generateRandomString();
   };
 
+
+  const generateRandomString = () => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let result1 = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 6; i++) {
+      result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    setNombreP(result1);
+  }
+
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={3}>
-        <Grid item xs>
+    <Box sx={{ justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Match para perros</h1>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-around"
+        alignItems="flex-start"
+        spacing={1}
+      >
+        <Grid item xs={3}>
+          <h1>Perros rechazados</h1>
           <List>
-            {rechazado.map(
-              (
-                item,
-                index // Parentesis
-              ) => (
-                <ListItem key={index}>
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image={rechazado[index]}
-                    alt="Paella dish"
-                  />
-                </ListItem>
-              )
-            )}
+            {rechazado.link.map((item, index) => (
+              <Stack key={index} marginBottom={2}>
+                <Card >
+                  <Stack direction={"row"}>
+                    <CardMedia
+                      component="img"
+                      height={100}
+                      style={{ maxWidth: '30%' }}
+                      className="card-media" //Aplicar la clase de estilo al componente
+                      image={rechazado.link[index]}
+                    />
+                    <h1>{aceptado.name[index]}</h1>
+                    <CardActions direction="row" spacing={2} ml={'25%'}>
+                      <Button variant="contained" color="success">Arrepentirse</Button>
+                    </CardActions>
+                  </Stack>
+                </Card>
+              </Stack>
+
+            ))}
           </List>
         </Grid>
 
-        <Grid item xs={6} centered>
-          <CardMedia
-            component="img"
-            height="400"
-            image={imagenPerro}
-            alt="Perro aleatorio"
-          />
-          <Stack direction="row" spacing={2}>
-            <Button variant="contained" color="success" onClick={() => aceptarP(imagenPerro)}>
-              ACEPTAR
-            </Button>
-            <Button variant="outlined" color="error" onClick={() => rechazarP(imagenPerro)}>
-              RECHAZAR
-            </Button>
+
+        <Grid item xs={3}>
+          <Stack alignItems="center" bgcolor={"white"} borderRadius={5}>
+
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {NombreP}
+              </Typography>
+            </CardContent>
+
+            <Card sx={{ maxWidth: 600 }}>
+              <CardMedia
+                component="img"
+                className="card-media"
+                height={500}
+                image={imagenPerro}
+                alt="Perro aleatorio"
+              />
+
+
+            </Card>
+            <CardActions direction="row" spacing={2} ml={'25%'}>
+              <Button variant="contained" color="success" onClick={() => aceptarP(imagenPerro, NombreP)}>
+                ACEPTAR
+              </Button>
+              <Button variant="outlined" color="error" onClick={() => rechazarP(imagenPerro, NombreP)}>
+                RECHAZAR
+              </Button>
+            </CardActions>
           </Stack>
+
         </Grid>
-        <Grid item xs>
+
+        <Grid item xs={3}>
+          <h1>Perros aceptados</h1>
           <List>
-            {aceptado.map(
-              (
-                item,
-                index // Parentesis
-              ) => (
-                <ListItem key={index}>
-                  <CardMedia
-                    component="img"
-                    height="194"
-                    image={aceptado[index]}
-                    alt="Paella dish"
-                  />
-                </ListItem>
-              )
-            )}
+            {aceptado.link.map((item, index) => (
+              <Stack key={index} marginBottom={2} >
+                <Card key={index}>
+                  <Stack direction={"row"}>
+                    <CardMedia
+                      component="img"
+                      height={100}
+                      style={{ maxWidth: '30%' }}
+                      className="card-media" //Aplicar la clase de estilo al componente
+                      image={aceptado.link[index]}
+                    />
+                    <h1>{aceptado.name[index]}  </h1>
+                    <CardActions direction="row" spacing={2} ml={'25%'}>
+                      <Button variant="contained" color="success">Arrepentirse</Button>
+                    </CardActions>
+
+                  </Stack>
+
+
+                </Card>
+              </Stack>
+
+            ))}
           </List>
         </Grid>
       </Grid>
-    </Box>
+    </Box >
   )
 }
 
